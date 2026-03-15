bef6bda6f3f798d1c32a0fa9844e17b2ffc12f3b
Nguyen Huy <huy246302@gmail.com>
2026-03-15 20:49:34 +0700

    add-supabase-utilities

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,6 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export function createClient() {
+  // Create a supabase client on the browser with project's credentials
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  );
+}
