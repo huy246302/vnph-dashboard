e1d61fec36ecbf5eabb64a8398ed5cfc3ab632d0
huy246302 <huy246302@gmail.com>
2026-06-27 19:00:18 +0700

    add console log createClient

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,7 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("Anon key exists:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 export function createClient() {
-  // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
